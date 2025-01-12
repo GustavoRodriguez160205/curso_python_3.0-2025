75d55018017db00c2cf3919772d6c623985a210c
GustavoRodriguez160205 <gustavorodriguezdeveloper@gmail.com>
2025-01-12 03:50:39 -0300

    Create 14 - Excepciones.py

---
+++ Modulo 1/14-Excepciones.py
@@ -0,0 +1,6 @@
+# Que son los eventos?
+
+# Es cualquier evento que sucede en nuestro programa, puede ser como hacer un click
+# mover el mouse o tocar cualquier letra.
+# Las excepciones son eventos que ocurren a lo largo de nuestro programa.
+# Es algo que hace que se detenga toda la ejecución de nuestro programa.
