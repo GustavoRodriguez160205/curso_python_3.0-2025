6d80aa67b22bf54255caa14289b3de3c0df389f6
GustavoRodriguez160205 <gustavorodriguezdeveloper@gmail.com>
2025-01-14 15:44:41 -0300

    Update 01 - Practicas.py

--- Modulo 1/Ejercicios/01-Practica.py
+++ Modulo 1/Ejercicios/01-Practica.py
@@ -1,61 +1,40 @@
-# 01. Sumar dos números
-# Pide al usuario que ingrese dos números y muestra la suma.
-
-numero1 = int(input("Ingresa un número:"))
-numero2 = int(input("Ingresa un número:"))
-print(f"La suma es: {numero1 + numero2}")
 
 
-
-# 02. Mayor de dos números
-# Pide dos números y muestra cuál es mayor o si son iguales.
-
-numero1 = int(input("Dame un número: "))
-numero2 = int(input("Dame otro número: "))
-
-if numero1 > numero2:
-    print(f"El primer número es mayor: {numero1}")
-elif numero1 == numero2:
-    print("Los núemros son iguales.")
-
-else:
-    print("No se ingresaron números.")
+# 1. Declara una variable llamada 'nombre' y asígnale tu nombre. Luego, imprime un saludo personalizado usando esta variable.
+nombre = "Gustavo"
+print(f"Hola, ¿cómo estás, {nombre}?")
 
 
-# 03. Par o impar
-# Pide un número y muestra si es par o impar.
-
-
-numero1 = int(input("Dame un número y te diré si es par o no: "))
-es_par = lambda x: x % 2 == 0 
-print(f"Es número par?: {es_par(numero1)}") 
+# 2. Calcula el área de un triángulo. Solicita al usuario que ingrese la base y la altura, y utiliza la fórmula: área = (base * altura) / 2.
+base = float(input("Ingresa la base del triángulo para calcular su área: "))
+altura = float(input("Ingresa la altura del triángulo para calcular su área: "))
+calculo = (base * altura) / 2
+print(f"El área del triángulo es: {calculo}")
 
 
 
-
-# 04. Conversor de temperatura
-# Convierte una temperatura de Celsius a Fahrenheit.
-
-numero1 = float(input("Ingresa una temperatura en Celsius y la convertiremos a Fahrenheit: "))
-conversion = (numero1 * 9/5) + 32
-print(f"La temperatura en Fahrenheit es: {conversion}")
-
-
+# 3. Escribe un programa que solicite al usuario tres números y calcule la media aritmética de esos números.  
+# Pista: La media aritmética se calcula como: (n1 + n2 + n3) / 3.
+n1 = int(input("Dame el primer número: "))
+n2 = int(input("Dame el segundo número: "))
+n3 = int(input("Dame el tercer número: "))
+resultado = (n1 + n2 + n3) / 3
+print(f"La media aritmética de los números es: {resultado}")
 
 
-# 05. Número positivo o negativo
-# Pide un número y muestra si es positivo, negativo o cero.
+# 4. Crea una variable frase que contenga una oración. Declara otra variable que cuente cuántos espacios hay en esa frase usando el método count().
+frase =     "Hola a todos como andan  . Aguante Milei carajooo    "
+contarEspacios = frase.count("  ")
+print(f"Los espacios que hay en esta frase son: {contarEspacios}")
 
-numero1 = int(input(" Decime un número y te dire si es positivo , negativo o cero. "))
 
-if numero1 > 0:
-    print("El número es positivo.")
 
-elif numero1 < 0:
-    print("El número es negativo.")
+# 5. Calcula el área de un círculo con un radio dado por el usuario.
+# Pista: Usa la fórmula área = π * r^2 (usa 3.1416 para π).
+radio = float(input("Por favor dame el radio del círculo: "))
+pi = 3.1416
 
-elif numero1 == 0:
-    print("El número es neutro.")
+area = pi * radio ** 2
+print(f"El área de un círculo con radio {radio} es: {area:.2f}")
 
-else:
-    print("Se termino.")
+
